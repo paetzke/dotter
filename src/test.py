ca70fcc304cc4e8fb124b923bb0460cedd802fd2
Friedrich Paetzke <f.paetzke@gmail.com>
2013-03-13 10:25:04 +0000

    updated example

--- src/test.py
+++ src/test.py
@@ -3,9 +3,10 @@
 from pydot import Dotter
 
 dotter = Dotter()
-dotter.setLink('n1', 'n2', '1To2')
+dotter.setLink('n1', 'n2', label='1To2')
 dotter.setLink('n3', 'n2')
 dotter.setLink('n1', 'n4')
+dotter.setLink('n4', 'n2')
 dotter.setLink('n4', 'n2')
 dotter.setLabel('n4', 'NFour')
 dotter.setShape('n2', Dotter.SHAPE_TRIANGLE)
