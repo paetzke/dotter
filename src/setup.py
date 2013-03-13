feb205764be598ee631f86d44322b91b29e81ca0
Friedrich Paetzke <f.paetzke@gmail.com>
2013-03-13 10:25:54 +0000

    updated version

--- src/setup.py
+++ src/setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(name='pydot',
       description='A Dot/Graphviz Wrapper',
-      version='0.1',
+      version='0.1.1',
       author='Friedrich Paetzke',
       author_email='f.paetzke@gmail.com',
       py_modules=['pydot'],
