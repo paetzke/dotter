440df9c0d70585f6e22badf31fa0061e493e4edb
Friedrich Paetzke <f.paetzke@gmail.com>
2013-06-04 23:01:22 +0200

    revised doc

--- src/pydot.py
+++ src/pydot.py
@@ -1,6 +1,6 @@
 '''
 @author: Friedrich Paetzke <f.paetzke@gmail.com>
-@copyright: Copyright (c) 2013 Friedrich Paetzke
+@copyright: Copyright (c) 2013, Friedrich Paetzke
 @license: GPL
 '''
 
@@ -9,9 +9,7 @@ import subprocess
 
 
 def _esc(s):
-    '''
-    @type s: str
-    '''
+    ' Returns the escaped string '
     s = base64.b16encode(bytes(s, encoding='utf-8')).decode(encoding='utf-8')
     for i in range(10):
         s = s.replace(chr(ord('0') + i), chr(ord('a') + i))
@@ -20,21 +18,15 @@ def _esc(s):
 
 class Dotter:
 
-    '''
-    '''
     GRAPH_DIRECTED = 0
     GRAPH_UNDIRECTED = 1
 
-    '''
-    '''
     SHAPE_BOX = 'box'
     SHAPE_CIRCLE = 'circle'
     SHAPE_FOLDER = 'folder'
     SHAPE_PLAINTEXT = 'plaintext'
     SHAPE_TRIANGLE = 'triangle'
 
-    '''
-    '''
     OUTPUT_BMP = 'bmp'
     OUTPUT_DOT = 'dot'
     OUTPUT_JPG = 'jpg'
@@ -48,12 +40,6 @@ class Dotter:
                  outputType=OUTPUT_PDF,
                  outputFilename=None,
                  isStrict=False):
-        '''
-        @type graphType:
-        @type outputType:
-        @type outputFilename: str
-        @type isStrict: bool
-        '''
         self._graphType = graphType
         self._isFirstCmd = True
         self._isStrict = isStrict
@@ -66,19 +52,18 @@ class Dotter:
             args.append('-o')
             args.append(outputFilename)
 
-        self.process = subprocess.Popen(args,
-                                        stdout=subprocess.PIPE,
-                                        stdin=subprocess.PIPE,
-                                        universal_newlines=True)
+        self.process = subprocess.Popen(
+            args,
+            stdout=subprocess.PIPE,
+            stdin=subprocess.PIPE,
+            universal_newlines=True)
 
     def close(self):
-        ''' Clean Up. '''
+        ' Clean up '
         self.execute('}')
 
-    def execute(self, cmd):
-        '''
-        @type cmd: str
-        '''
+    def execute(self, cmd: str):
+        ' Executes the command '
         if self._isFirstCmd:
             self._isFirstCmd = False
             if self._isStrict:
@@ -93,30 +78,19 @@ class Dotter:
         self.process.stdin.write(cmd)
 
     def setLabel(self, node, label):
-        '''
-        @type node: str
-        @type label: str
-        '''
+        ' Sets the label for node '
         self.execute('{0} [label="{1}"]'.format(_esc(node), label))
 
     def setLink(self, node1, node2, label=None):
-        '''
-        @type node1: str
-        @type node2: str
-        @type label: str
-        '''
+        ' Links node1 with node2 '
         if self._graphType == Dotter.GRAPH_DIRECTED:
             fmt = '{0} -> {1}'
         else:
             fmt = '{0} -- {1}'
-
         if label is not None:
             fmt += ' [label="{0}"]'.format(label)
         self.execute(fmt.format(_esc(node1), _esc(node2)))
 
     def setShape(self, node, shape):
-        '''
-        @type node: str
-        @type shape:
-        '''
+        ' Sets the shape for node  '
         self.execute('{0} [shape="{1}"]'.format(_esc(node), shape))
