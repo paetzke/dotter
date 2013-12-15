77fa54fdf2152dda8c5d4c0a980d8cfd7104c444
Friedrich Paetzke <f.paetzke@gmail.com>
2013-12-15 16:53:10 +0100

    Add test for =Dotter.rank()=

--- test/test.py
+++ test/test.py
@@ -13,7 +13,13 @@ import unittest
 BASE_DIR = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
 sys.path.insert(0, BASE_DIR)
 
-from dotter import Dotter
+from dotter import Dotter, RankType
+
+
+def load_data(filename):
+    filename = os.path.join(BASE_DIR, 'test/data', filename)
+    with open(filename, 'r') as f:
+        return f.read().splitlines()
 
 
 class TestDotter(unittest.TestCase):
@@ -41,6 +47,21 @@ class TestDotter(unittest.TestCase):
         dotter = Dotter()
         dotter.add_node('a', label='b')
         expected = ['digraph', ' {', 'gb', 'gb [label="b"]']
+        self.assertEqual(expected, dotter.commands)
+
+    def test_rank(self):
+        dotter = Dotter()
+        for node in ['a', 'b', 'c', 'd']:
+            dotter.add_node(node, label=node)
+        dotter.add_edge('a', 'b')
+        dotter.add_edge('a', 'c')
+        dotter.add_edge('a', 'd')
+
+        dotter.rank(['a', 'b'], RankType.Min)
+        dotter.rank(['c'], RankType.Same)
+        dotter.rank(['d'], RankType.Max)
+
+        expected = load_data('test_rank.dot')
         self.assertEqual(expected, dotter.commands)
 
 
