8c63a42e4d3a63e002ebe35e61cbc47e9bab68f0
Friedrich Paetzke <f.paetzke@gmail.com>
2014-01-19 10:42:06 +0100

    Bump version: 0.0.4 → 0.1.0

--- dotter/__init__.py
+++ dotter/__init__.py
@@ -8,7 +8,7 @@ All rights reserved.
 """
 from .dotter import Dotter, RankType, Shape
 
-__version__ = '0.0.4'
+__version__ = '0.1.0'
 __author__ = 'Friedrich Paetzke'
 __license__ = 'BSD'
 __copyright__ = 'Copyright 2013 Friedrich Paetzke'
